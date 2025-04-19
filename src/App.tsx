1ab5c752c8956278eca90cb197625ed67e79bbc8
Anweshasaha20 <anweshasaha192@gmail.com>
2025-04-19 11:48:48 +0530

    Refactor routing in App component to use correct page components for skills and projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Portfolio</div>} />
-          <Route path="/about" element={<div>About</div>} />
-          <Route path="/skills" element={<div>Projects</div>} />
-          <Route path="/projects" element={<div>Skills</div>} />
-          <Route path="/contact" element={<div>Contact</div>} />
+          <Route path="/" element={} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Projects />} />
+          <Route path="/projects" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
