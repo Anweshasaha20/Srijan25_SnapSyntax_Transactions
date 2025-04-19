0b24448d760cae54f53cf5663379570c8f711bad
Anweshasaha20 <anweshasaha192@gmail.com>
2025-04-19 11:48:54 +0530

    abc

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
-import Skills from "./pages/Skills";
-
+import Skills from "./pages/skills";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Projects />} />
           <Route path="/projects" element={<Skills />} />
