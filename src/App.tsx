6bd19dab6b327efff48efbf55f9c09957ae575b9
dasakash26 <akashd2664@gmail.com>
2025-04-19 11:17:01 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Portfolio</div>
+      <Button onClick={() => setCount(count + 1)}>Count: {count}</Button>
+    </>
+  );
+}
+
+export default App;
